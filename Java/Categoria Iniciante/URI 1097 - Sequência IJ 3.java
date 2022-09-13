3eca6094f7183f6e4daec649a3a435887a3ff124
Edgar Sangali <edgarsangali@outlook.com>
2022-09-13 07:50:00 -0300

    upload dos códigos fonte URI 1096 e 1097 - Java

---
+++ Java/Categoria Iniciante/URI 1097 - Sequência IJ 3.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		int i = 1, j = 7;
+
+		while (i <= 9) {
+
+			System.out.printf("I=%d J=%d\n", i, j);
+			System.out.printf("I=%d J=%d\n", i, j - 1);
+			System.out.printf("I=%d J=%d\n", i, j - 2);
+
+			i += 2;
+			j += 2;
+
+		}
+
+	}
+
+}
