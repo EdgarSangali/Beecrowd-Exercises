ffcd378697bb2d2e71c074844dbdb68073d33d91
Edgar Sangali <edgarsangali@outlook.com>
2022-09-19 09:45:26 -0300

    Upload do exercício URI 1115 em Java

---
+++ Java/Categoria Iniciante/URI 1115 - Quadrante.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int X = 0;
+		int Y = 0;
+
+		while (((X = sc.nextInt()) != 0) && ((Y = sc.nextInt()) != 0)) {
+
+			if (X > 0 && Y > 0) {
+				System.out.println("primeiro");
+			} else if (X < 0 && Y > 0) {
+				System.out.println("segundo");
+			} else if (X < 0 && Y < 0) {
+				System.out.println("terceiro");
+			} else {
+				System.out.println("quarto");
+			}
+		}
+
+		sc.close();
+	}
+
+}
