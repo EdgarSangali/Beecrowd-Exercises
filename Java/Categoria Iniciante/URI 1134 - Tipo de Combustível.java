c6a344aa3ae265d9755aa5b8950ec8b7a2f7e171
Edgar Sangali <edgarsangali@outlook.com>
2022-09-26 09:14:26 -0300

    Upload do código URI 1134 em Java

---
+++ Java/Categoria Iniciante/URI 1134 - Tipo de Combustível.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int gasolina = 0, alcool = 0, diesel = 0, opcao = 0;
+
+		while (opcao != 4) {
+			opcao = sc.nextInt();
+
+			while (opcao < 1 || opcao > 4) {
+				opcao = sc.nextInt();
+			}
+
+			switch (opcao) {
+
+			case 1:
+				alcool++;
+				break;
+
+			case 2:
+				gasolina++;
+				break;
+
+			case 3:
+				diesel++;
+				break;
+			}
+
+		}
+
+		System.out.println("MUITO OBRIGADO");
+		System.out.println("Alcool: " + alcool);
+		System.out.println("Gasolina: " + gasolina);
+		System.out.println("Diesel: " + diesel);
+
+		sc.close();
+
+	}
+
+}
