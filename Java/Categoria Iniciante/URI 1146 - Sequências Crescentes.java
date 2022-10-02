8d29e70a2b880054fab84d6294886ac37e596c51
Edgar Sangali <edgarsangali@outlook.com>
2022-10-02 11:27:17 -0300

    Upload do código URI 1146 - Java

---
+++ Java/Categoria Iniciante/URI 1146 - Sequências Crescentes.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int X;
+		Scanner sc = new Scanner(System.in);
+		while ((X = sc.nextInt()) != 0) {
+			for (int i = 1; i <= X; i++) {
+				if (i == X) {
+					System.out.print(i + "\n");
+				} else {
+					System.out.print(i + " ");
+				}
+			}
+		}
+	}
+}
