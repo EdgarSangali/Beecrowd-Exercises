48808c34d91ed5ec9ab7aae9d5e8ff91afb279a7
Edgar Sangali <edgarsangali@outlook.com>
2022-10-11 09:38:55 -0300

    Upload dos códigos URI 1156 (Java) e URI 1002 (C#)

---
+++ C#/Categoria Iniciante/URI 1002 - Área do Círculo.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+double pi, raio, raioQuadrado, area;
+
+pi = 3.14159;
+raio = double.Parse(Console.ReadLine(),CultureInfo.CreateSpecificCulture("en-US"));
+raioQuadrado = Math.Pow(raio,2);
+area = raioQuadrado*pi;
+
+Console.WriteLine("A=" + area.ToString("F4", CultureInfo.CreateSpecificCulture("en-US")));
+Console.Read();
