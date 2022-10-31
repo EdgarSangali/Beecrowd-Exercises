7af9da1e8d01ef6bb3e52577da6a83bc30dad5b0
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2022-10-31 11:12:32 -0300

    submissão dos códigos URI 1014 e 1015 (C#) e 1176 (Java)

---
+++ C#/Categoria Iniciante/URI 1014 - Consumo.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+int X = int.Parse(Console.ReadLine());
+double Y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+double consumo = X/Y;
+
+Console.WriteLine(consumo.ToString("F3",CultureInfo.InvariantCulture) + " km/l");
+Console.Read();
+
+
+
