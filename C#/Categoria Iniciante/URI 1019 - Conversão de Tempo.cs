c489ca6b732f8b27097cd95ff004a2aadd8fdb5b
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2022-11-07 08:32:13 -0300

    upload do código URI 1019 (C#)

---
+++ C#/Categoria Iniciante/URI 1019 - Conversão de Tempo.cs
@@ -0,0 +1,14 @@
+﻿int N, hora, minuto, segundo, resto;
+
+N = int.Parse(Console.ReadLine());
+
+hora = N / 3600;
+resto = N % 3600;
+
+minuto = resto / 60;
+resto = resto % 60;
+
+segundo = resto;
+
+Console.WriteLine(hora + ":" + minuto + ":" + segundo);
+Console.Read();
