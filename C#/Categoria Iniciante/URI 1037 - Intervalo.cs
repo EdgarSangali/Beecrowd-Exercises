c396fc5f622a792131c9a80d23858d05570c9ea3
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2022-11-22 10:48:10 -0300

    Upload do código URI 1037 (C#)

---
+++ C#/Categoria Iniciante/URI 1037 - Intervalo.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+double intervalo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+if (intervalo > 75.00 && intervalo <=100.00) {
+    Console.WriteLine("Intervalo (75,100]");
+    Console.Read();
+}
+
+else if (intervalo > 50.00 && intervalo <=75.00) {
+    Console.WriteLine("Intervalo (50,75]");
+    Console.Read();
+}
+
+else if (intervalo > 25.00 && intervalo <=50.00) {
+    Console.WriteLine("Intervalo (25,50]");
+    Console.Read();
+}
+
+else if (intervalo >= 0.00 && intervalo <=25.00) {
+    Console.WriteLine("Intervalo [0,25]");
+    Console.Read();
+}
+
+else {
+  Console.WriteLine("Fora de intervalo");
+    Console.Read();  
+}
