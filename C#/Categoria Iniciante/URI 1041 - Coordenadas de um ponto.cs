cbc668d487d1e20d4e9a2cd090dc07a3e817e65e
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2022-11-30 09:45:21 -0300

    Upload do código URI 1041 (C#)

---
+++ C#/Categoria Iniciante/URI 1041 - Coordenadas de um ponto.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+string [] coordenadas = Console.ReadLine().Split(' ');
+double x = double.Parse(coordenadas[0],CultureInfo.InvariantCulture);
+double y = double.Parse(coordenadas[1],CultureInfo.InvariantCulture);
+
+if (x == 0 & y==0) 
+{
+    Console.WriteLine("Origem");
+    Console.Read();
+}
+
+else if (x != 0 & y==0) 
+{
+    Console.WriteLine("Eixo X");
+    Console.Read();
+}
+
+else if (x == 0 & y!=0) 
+{
+    Console.WriteLine("Eixo Y");
+    Console.Read();
+}
+
+else if (x > 0 & y > 0) 
+{
+    Console.WriteLine("Q1");
+    Console.Read();
+}
+
+else if (x < 0 & y > 0) 
+{
+    Console.WriteLine("Q2");
+    Console.Read();
+}
+
+else if (x < 0 & y < 0) 
+{
+    Console.WriteLine("Q3");
+    Console.Read();
+}
+
+else 
+{
+    Console.WriteLine("Q4");
+    Console.Read();
+}
