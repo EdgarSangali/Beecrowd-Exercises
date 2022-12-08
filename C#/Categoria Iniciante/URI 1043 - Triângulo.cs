80a3a2dabe4087bc522f49728fe12701545d2e20
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2022-12-08 07:52:43 -0300

    Upload do código URI 1043 (C#)

---
+++ C#/Categoria Iniciante/URI 1043 - Triângulo.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+string [] dimensoes = Console.ReadLine().Split(' ');
+double A = double.Parse(dimensoes[0],CultureInfo.InvariantCulture);
+double B = double.Parse(dimensoes[1],CultureInfo.InvariantCulture);
+double C = double.Parse(dimensoes[2],CultureInfo.InvariantCulture);
+double perimetro = A + B + C;
+double area = ((A + B)* C) / 2;
+
+if((A < B + C) && ( B < A + C) && (C < A + B))
+{
+    Console.WriteLine("Perimetro = " + perimetro.ToString("F1",CultureInfo.InvariantCulture));
+    Console.Read();
+}
+
+else
+{
+    Console.WriteLine("Area = " + area.ToString("F1",CultureInfo.InvariantCulture));
+    Console.Read();
+}
+
