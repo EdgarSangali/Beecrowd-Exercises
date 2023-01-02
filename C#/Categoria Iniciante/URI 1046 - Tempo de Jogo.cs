f36b125730041e5f974103911fde6a8fe819630f
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2023-01-02 10:31:55 -0300

    Upload do código URI 10446 (C#)

---
+++ C#/Categoria Iniciante/URI 1046 - Tempo de Jogo.cs
@@ -0,0 +1,16 @@
+﻿string [] entradas = Console.ReadLine().Split(' ');
+int inicio = int.Parse(entradas[0]);
+int final = int.Parse(entradas[1]);
+int duracao = 0;
+
+if (inicio >= final)
+{
+    duracao = (24 + final) - inicio;
+}
+
+else
+{
+    duracao = (final - inicio);
+}
+
+Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
