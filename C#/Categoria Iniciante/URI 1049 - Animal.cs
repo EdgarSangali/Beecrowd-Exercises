96031b98ada2fec358946ba5205034e5d9b15f5a
Edgar Sangali <106881710+EdgarSangali@users.noreply.github.com>
2023-01-15 19:28:50 -0300

    Upload do código URI 1049 (C#)

---
+++ C#/Categoria Iniciante/URI 1049 - Animal.cs
@@ -0,0 +1,54 @@
+﻿string tipo = Console.ReadLine();
+string especie = Console.ReadLine();
+string alimentacao = Console.ReadLine();
+
+if (tipo == "vertebrado")
+{
+    if(especie == "ave") 
+    {
+        if(alimentacao == "carnivoro")
+        {
+            Console.WriteLine("aguia");
+        }
+        else if(alimentacao == "onivoro")
+        {
+            Console.WriteLine("pomba");
+        }
+    }
+    else if (especie == "mamifero")
+    {
+        if (alimentacao == "onivoro")
+        {
+            Console.WriteLine("homem");
+        }
+        else if (alimentacao == "herbivoro")
+        {
+            Console.WriteLine("vaca");
+        }
+    }
+}
+    else if (tipo == "invertebrado")
+    {
+        if (especie == "inseto")
+        {
+            if(alimentacao == "hematofago")
+            {
+                Console.WriteLine("pulga");
+            }
+            else if (alimentacao == "herbivoro")
+            {
+                Console.WriteLine("lagarta");
+            }
+        }
+        else if( especie == "anelideo")
+        {
+            if(alimentacao == "hematofago")
+            {
+                Console.WriteLine("sanguessuga");
+            }
+            else if(alimentacao == "onivoro")
+            {
+                Console.WriteLine("minhoca");
+            }
+        }
+    }
